61319ac6418097b72bb5b562c421bee81e5b8892
Will Nemoy <98560701+WillNemoy@users.noreply.github.com>
2022-12-15 15:49:53 -0500

    Fix Excel output

--- app.py
+++ app.py
@@ -201,13 +201,13 @@ def facebookAPI(token, pageInput):
     #for printing the post data
     #for i in range(len(postList)):
     #    printPostData(postList[i])
-        
-    
+
     #create output Excel file
+    file_name = pageInput + " Facebook Data.xlsx"
     excelFile = Workbook()
-    excelFile.save(pageInput + " Facebook Data.xlsx")
+    excelFile.save(file_name)
         
-    excelFile = load_workbook(pageInput + " Facebook Data.xlsx")
+    excelFile = load_workbook(file_name)
     sheet = excelFile.active
     
     #sheet name
@@ -228,7 +228,7 @@ def facebookAPI(token, pageInput):
     sheet.cell(row = 1, column = 12).value = "Words" 
     sheet.cell(row = 1, column = 13).value = "Hashtags" 
     sheet.cell(row = 1, column = 14).value = "Comments Text" 
-    excelFile.save(pageInput + " Facebook Data.xlsx")
+    excelFile.save(file_name)
     
     #adjust column widths
     sheet.column_dimensions['A'].width = 8.26
@@ -246,7 +246,7 @@ def facebookAPI(token, pageInput):
     sheet.column_dimensions['M'].width = 8.26
     sheet.column_dimensions['N'].width = 15.53
     
-    excelFile.save(pageInput + " Facebook Data.xlsx")
+    excelFile.save(file_name)
     
     #print post data to the Excel file
     lastRowFree = 2
@@ -315,7 +315,9 @@ def facebookAPI(token, pageInput):
             sheet.cell(row = lastRowFree, column = 10).value = postList[i].getShares()
             lastRowFree += 1
                 
-    excelFile.save(pageInput + " Facebook Data.xlsx")
+    excelFile.save(file_name)
+
+
 
 #run the function
 FACEBOOK_API_KEY = os.getenv("FACEBOOK_API_KEY")
@@ -323,69 +325,3 @@ FACEBOOK_API_KEY = os.getenv("FACEBOOK_API_KEY")
 user_account_search = input("Please enter a Facebook business page: ")
 
 facebookAPI(FACEBOOK_API_KEY, user_account_search)
-
-
-
-
-
-
-"""
-def yelpAPI(YELP_API_KEY_parameter, search_term, location_parameter):
-    
-    with YelpAPI(YELP_API_KEY_parameter, timeout_s = 3.0) as yelp_api:
-        data = yelp_api.search_query(term=search_term, location=location_parameter, limit=50)
-
-    #Sheet 1 Consolidated Code
-    df = pd.DataFrame(data["businesses"])
-
-    df = df.reset_index()
-
-    def city(x):
-        city = x["city"]
-        return city
-
-    def zip_code(x):
-        zip_code = x["zip_code"]
-        return zip_code
-
-    df["city"] = df["location"].apply(lambda x: city(x))
-    df["zip_code"] = df["location"].apply(lambda x: zip_code(x))
-
-    def lat(x):
-        latitude = x["latitude"]
-        return latitude
-
-    def long(x):
-        longitude = x["longitude"]
-        return longitude
-
-    df["latitude"] = df["coordinates"].apply(lambda x: lat(x))
-    df["longitude"] = df["coordinates"].apply(lambda x: long(x))
-
-
-    #Sheet 2 Consolidated Code
-    df_sheet2 = df[['index', 'categories']]
-
-    categories_list = []
-    for i in range(len(df_sheet2)):
-        for x in range(len(df_sheet2['categories'].loc[i])):
-            categories = [i, df_sheet2['categories'].loc[i][x]['title']]
-            categories_list.append(categories)
-            
-    df_sheet2 = pd.DataFrame(categories_list)
-    df_sheet2 = df_sheet2.rename(columns={0:"Business Id", 1:"Categories"})
-
-    excel_file_name = "Yelp Data" + ", " + location_parameter + ", " + search_term + ".xlsx"
-    #to an Excel file!
-    with pd.ExcelWriter(excel_file_name) as writer:  
-        df.to_excel(writer, sheet_name='Data')
-        df_sheet2.to_excel(writer, sheet_name='Categories')
-
-#run the function
-YELP_API_KEY = os.getenv("YELP_API_KEY")
-
-user_search_term = input("What type of businesses are you interested in? ")
-user_location = input("What area should we focus on? ")
-
-yelpAPI(YELP_API_KEY, user_search_term, user_location)
-"""
